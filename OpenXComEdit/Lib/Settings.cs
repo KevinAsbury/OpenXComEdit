5513f362689c40436da638864ede62e2bbddc2c5
Kevin Asbury <kevinasbury.dev@gmail.com>
2021-01-24 19:00:39 -0500

    add settings file

---
+++ OpenXComEdit/Lib/Settings.cs
@@ -0,0 +1,20 @@
+﻿namespace OpenXComEdit.Lib
+{
+    public class Settings
+    {
+        public string OpenXcomPath { get; set; }
+        public string SavePath { get; set; }
+
+        public Settings(string openXcomPath, string savePath)
+        {
+            OpenXcomPath = openXcomPath;
+            SavePath = savePath;
+        }
+
+        public Settings()
+        {
+            OpenXcomPath = "";
+            SavePath = "";
+        }
+    }
+}
--- OpenXComEdit/Program.cs
+++ OpenXComEdit/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenXComEdit.Lib;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace OpenXComEdit
 {
@@ -17,7 +21,37 @@ namespace OpenXComEdit
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            LoadSettings();
             Application.Run(new Form1());
+        }
+
+        internal static Settings Settings { get; set; }
+
+        static void LoadSettings()
+        {
+            if (File.Exists("Settings.yaml"))
+            {
+                var yaml = File.ReadAllText("Settings.yaml");
+
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .IgnoreUnmatchedProperties()
+                    .Build();
+
+                Settings = deserializer.Deserialize<Settings>(yaml);
+            }
+            else
+            {
+                var settings = new Settings();
+                var serializer = new SerializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull)
+                    .DisableAliases()
+                    .Build();
+                var yaml = serializer.Serialize(settings);
+
+                File.WriteAllText("Settings.yaml", yaml);
+            }
         }
     }
 }
